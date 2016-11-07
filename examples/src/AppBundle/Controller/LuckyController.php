8137ce529f25ad4b2df934037f367797a0c1b3bb
Gene Rhim <generhim@gmail.com>
2016-11-07 13:13:12 -0800

    Beginning to add twig example

--- examples/src/AppBundle/Controller/LuckyController.php
+++ examples/src/AppBundle/Controller/LuckyController.php
@@ -9,6 +9,10 @@
     // src/AppBundle/Controller/LuckyController.php
     namespace AppBundle\Controller;
 
+    // we use the FrameworkBundle\Controller\Controller in order to use the twig templating language.
+    use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
     use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
     use Symfony\Component\HttpFoundation\Response;
 
@@ -25,6 +29,20 @@
                 '<html><body>Lucky number: '.$number.'</body></html>'
             );
         }
+
+
+        /**
+         * @Route("/lucky/twig_number")
+         */
+        public function twig_numberAction()
+        {
+            $number = mt_rand(0, 100);
+
+            return new Response(
+                '<html><body>Lucky number: '.$number.'</body></html>'
+            );
+        }
+
     }
 
 /* End of Controller */
