1de0cd86439947c46a2f3a41028de19eb736ece8
Gene Rhim <generhim@gmail.com>
2016-11-09 20:00:08 -0800

    Adding rest examples using POST,GET,PUT, and DELETE with the Request and Response/JsonResponse objects.

---
+++ examples/src/AppBundle/Controller/ControlController.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Self
+ * Date: 11/7/16
+ * Time: 12:48 PM
+ */
+
+// src/AppBundle/Controller/LuckyController.php
+namespace AppBundle\Controller;
+
+// we use the FrameworkBundle\Controller\Controller in order to use the twig templating language.
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+// we are now extending the Controller base class to make the twig templating language available.
+// otherwise we will throw an error because $this->render() function will not be identified by the app.
+class ControlController extends Controller
+{
+    /**
+     * @Route("/control/")
+     */
+    public function numberAction()
+    {
+        $number = mt_rand(0, 100);
+
+        return new Response(
+            '<html><body>Lucky number: '.$number.'</body></html>'
+        );
+    }
+
+
+    /**
+     * @Route("/lucky/twig_number")
+     */
+    public function twig_numberAction()
+    {
+        $number = mt_rand(0, 100);
+
+        // this twig template is saved in the {my_app}/app/Resources/views/lucky/ directory...
+        return $this->render('lucky/twig_number.html.twig', array(
+                'number' => $number,
+            )
+        ); // end of $this->render
+    }
+
+}
+
+/* End of Controller */
---
+++ examples/src/AppBundle/Controller/RestController.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Self
+ * Date: 11/9/16
+ * Time: 5:17 PM
+ */
+
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+
+class RestController extends Controller
+{
+
+    /**
+     * Return a json object.
+     *
+     * @Route("/rest/") name="whatever"
+     */
+    public function rest_examplesAction(Request $request)
+    {
+
+        if($request->isMethod('POST'))
+        {
+            $content    = json_decode($request->getContent(),TRUE);
+            $response   = array('requestType' => 'POST',    'requestObj' => $request, 'requestContent' => $content);
+        }
+
+        if($request->isMethod('GET'))
+        {
+            $content        = array();
+            $content['pie'] = $request->query->get('pie');
+            $response       = array('requestType' => 'GET',     'requestObj' => $request, 'requestContent' => $content);
+        }
+
+        if($request->isMethod('PUT'))
+        {
+            $content    = json_decode($request->getContent(),TRUE);
+            $response   = array('requestType' => 'PUT',     'requestObj' => $request, 'requestContent' => $content);
+        }
+
+        if($request->isMethod('DELETE'))
+        {
+            $content    = json_decode($request->getContent(),TRUE);
+            $response   = array('requestType' => 'DELETE',  'requestObj' => $request, 'requestContent' => $content);
+        }
+
+
+        // In case you want to use the Response class. (Not so necessary.)
+        //$response = new Response(json_encode($response));
+        //$response->headers->set('Content-Type','application/json');
+        //return $response;
+
+        // In case you want to use the JsonResponse class instead. (Preferable)
+        return new JsonResponse($response);
+        
+
+
+    }
+}
+
